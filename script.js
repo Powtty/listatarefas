676a3b195721739476eb185408c0aea4d7380204
Prof. Fernando Tamberlini <fernando.alves@ifrj.edu.br>
2025-11-21 20:03:28 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+//Utilize este array para trabalhar com a lista de tarefas
+let tarefas = [];
+
+function adicionarLista(){
+    //Selecionando os elementos da página html input
+    const campoTarefa = document.getElementById("inputTarefa");
+    let textoTarefa = campoTarefa.value;
+
+    //Limpando e colocando em maíusculas   
+    // ***** Inclua o comando para tratar o texto aqui 
+    
+    //Verifique se a tarefa já foi criada!!!
+    //Adicionando no array tarefas
+    tarefas.push(textoTarefa);
+    
+    
+    //Chamando a função incluir tarefa
+    incluirTarefaLista(textoTarefa)
+       
+    //Limpando o campo de entrada da Tarefa após a inclusão na lista
+    // ***** Inclua o comando para limpar o campo aqui
+    
+   
+}
+
+function incluirTarefaLista(textoTarefa){
+
+    //Selecionando os elementos da página html input e lista
+    const listaTarefas = document.getElementById("listaTarefas");
+    
+    //Criando um novo elemento item-lista
+    const novaTarefa = document.createElement("li");
+    
+    //Incluindo o novo elemento na lista
+    listaTarefas.appendChild(novaTarefa);
+    
+    //Colocando a classe no novo item da lista
+    novaTarefa.classList.add("item-tarefa");  
+
+    //Incluindo o texto e botão de excluir no novo item da lista
+    novaTarefa.innerHTML = `
+        <span>${textoTarefa}</span>
+        <div class="divLixeira">
+            <img class="btnLixeira" onclick="excluirTarefa(this)" src="lixeira.png">
+        </div>`
+
+}
+
+function excluirTarefa(tarefa){
+    const tarefaClicada = tarefa.closest("li");
+    textoTarefa=tarefaClicada.textContent.trim();
+    tarefas = tarefas.filter(tarefa => tarefa != textoTarefa);
+    tarefaClicada.remove();
+}
+
